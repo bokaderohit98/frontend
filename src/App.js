23adc2e98bb2a349a19e1ed45cd3a8f0a1a977b4
bokaderohit98 <bokaderohit98@gmail.com>
2020-03-29 17:14:50 +0530

    feat(intro): changed intro

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          There is nothing like REACT
+          There is nothing like REACT!
         </p>
         <a
           className="App-link"
