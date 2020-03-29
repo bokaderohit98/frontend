57f421ff753b03c38f4bd0d2685bb878f495f182
bokaderohit98 <bokaderohit98@gmail.com>
2020-03-29 18:10:08 +0530

    feat(header): changed header

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          There is nothing like REACT!
+          First AWS Deployment
         </p>
         <a
           className="App-link"
